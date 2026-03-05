235984dea874266bac86717c04f14d40eccf9568
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-03-05 17:55:36 +0530

    Update numericOperations.py

--- numericOperations.py
+++ numericOperations.py
@@ -1,1 +1,30 @@
+a = 10
+b = 20
+c = 10.5
+d = 200.555
 
+#addition
+print(a+b)
+print(a+d)
+
+#Subtraction
+print(d-c)
+
+#Multiplication
+print(b*a)
+print(d*c)
+
+#Division - Returns the exact result in decimal (float).
+print(b/a)
+print(d/c)
+
+#Floor Division - (same as java division- returns quotient)
+print(b//a)
+print(d//c)
+
+#Modulus - Returns the remainder 
+print(b%a)
+print(d%c)
+
+#Exponent - Raises the left operand to the power of the right operand.
+print(2 ** 3)
