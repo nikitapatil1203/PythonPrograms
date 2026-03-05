569885b54cc3950d123031e290a2ffb42702c643
Nikita Patil <nikitapatil@Nikitas-MacBook-Air.local>
2026-03-05 16:19:48 +0530

    dataType

---
+++ dataType.py
@@ -0,0 +1,55 @@
+#List [] , tuple (), set {}, Dict {} , frozenSet ([])
+#1. int
+a = 10
+print(a)
+
+#2. float
+b = 10.5
+print(b)
+
+#3. complex
+c = 2 + 4j
+print(c)
+
+#4. String
+x = 'abc'
+print(x)
+c = "Nikita"
+print(c)
+y='''patil'''
+print(y)
+z = "nikita patil"
+print(z)
+print(z[0])     # can fetch individual characters like this
+
+#5. Set (can change the value - unique values)
+d = {1,2,3,4,5}
+print(d)
+
+#6. frozenset (can't change the value - unique values)
+e = frozenset([1,2,3,4,5])
+print(e)
+
+#7. List(can add duplicate - can change the values)
+g = [1,2,3,4,5,5]
+print(g)
+g[0] = 2
+print(g)
+
+#8. tuple (can't change the values)
+h = (1,2,3,4,5,5)
+print(h)
+
+#9. Dict (Key, value)
+abc = {"name":"nikita", "age": 2}
+print(abc)
+
+#10. Boolean
+xyz = False
+print(xyz)
+
+#11. Null
+a = ""
+print(a)
+
+
