4c923ca71afcdc272026c29d2e8d4e8b522f2857
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-03-05 17:18:14 +0530

    Update stringManipulation.py

--- stringManipulation.py
+++ stringManipulation.py
@@ -1,5 +1,17 @@
 #string-concat
-name = "nikita"
-surname = "patil"
-full_name = name + surname
+name = "Nikita"
+surname = "Patil"
+full_name = name+" "+surname
 print(full_name)
+
+#find length
+print(len(full_name))
+
+#lowercase
+lowerCaseString = full_name.lower()
+print(lowerCaseString)
+
+#replace
+text = "my name is nikita"
+newText = text.replace("nikita", "nikita patil")
+print(newText)
