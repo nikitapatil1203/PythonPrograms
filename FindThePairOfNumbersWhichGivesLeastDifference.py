f20b93475bf00e28b1c9105539b6b4bab3468600
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-06-11 17:13:20 +0530

    Update FindThePairOfNumbersWhichGivesLeastDifference.py

--- FindThePairOfNumbersWhichGivesLeastDifference.py
+++ FindThePairOfNumbersWhichGivesLeastDifference.py
@@ -1,18 +1,18 @@
 numList = [20,15,8,2,12,5]
 numList.sort()
-smallestdiff = numList[len(numList)-1]
-print(smallestdiff)
+smallestDiff = numList[len(numList)-1]
+print(smallestDiff)
 pair = []
 
 
 for i in range(0, len(numList)-1):
     for j in range(i+1, len(numList)-1):
         difference = abs(numList[i] - numList[j])
-        if smallestdiff > difference:
-            smallestdiff = difference
+        if smallestDiff > difference:
+            smallestDiff = difference
             pair = [(numList[i], numList[j])]
 
-        elif smallestdiff == difference:
+        elif smallestDiff == difference:
             pair.append((numList[i],numList[j]))
 
 print( smallestdiff)
