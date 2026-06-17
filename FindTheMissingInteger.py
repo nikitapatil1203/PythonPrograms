21a838e7d744cd6472e37ceecd11a477bb6a37dd
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-06-17 13:56:52 +0530

    Update FindTheMissingInteger.py

--- FindTheMissingInteger.py
+++ FindTheMissingInteger.py
@@ -1,14 +1,16 @@
 numlist = [1,10,3,5,6,9]
 numlist.sort()
+a = 0
 newString = []
 
-for i in range(1,numlist[len(numlist)-1]):
-    newString.append(i)
-    i+=1
+for i in range(1,numlist[-1]+1):
+    if i in numlist:
+        newString.append(numlist[a])
+        a += 1
+    else:
+        newString.append(i)
 
-newString.append(i)
 print(newString)
-
 
 
 
