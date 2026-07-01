f2caa38ad4c804e53151660265687f5ce28e927e
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-07-01 15:39:23 +0530

    Update Get_index_of_two_digit_which_gives_sum_as_nine.py

--- Get_index_of_two_digit_which_gives_sum_as_nine.py
+++ Get_index_of_two_digit_which_gives_sum_as_nine.py
@@ -1,1 +1,14 @@
+lst = [1,2,5,6,4,7]
+total = 0
+pairs = []
+
+
+for i in range(len(lst)):
+    for j in range (i,len(lst)):
+        total = lst[i]+lst[j]
+        if total == 9:
+            pairs.append((i,j))
+
+print(pairs)
+
 
