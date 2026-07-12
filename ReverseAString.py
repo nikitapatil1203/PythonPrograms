a31d3bd3f2d4bc91362d42002363b58bd9a3b1e4
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-07-12 16:12:58 +0530

    Update ReverseAString.py

--- ReverseAString.py
+++ ReverseAString.py
@@ -10,3 +10,16 @@ while i>=0:
 
 print(reversedString)
 
+
+
+# 2) reverse only words not characters
+inputString = "Hello, World! Welcome to Python.".split(" ")
+reversedString = ""
+i = len(inputString)-1
+
+while i>=0:
+    reversedString = reversedString + inputString[i] + " "
+    i -= 1
+
+print(reversedString.strip())
+
