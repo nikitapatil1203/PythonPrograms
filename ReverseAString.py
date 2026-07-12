ad74d4293f3b53da84e4e0426427dec8829e5b7d
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-07-12 16:05:51 +0530

    Update ReverseAString.py

--- ReverseAString.py
+++ ReverseAString.py
@@ -1,1 +1,12 @@
+# 1) reverse complete String
+
+inputString = "nikita"
+reversedString = ""
+i = len(inputString)-1
+
+while i>=0:
+    reversedString = reversedString+inputString[i]
+    i -= 1
+
+print(reversedString)
 
