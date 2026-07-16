96b37bb64264d7d97f1ec3da4114e605a64706c8
Nikita Patil <212394051+nikitapatil1203@users.noreply.github.com>
2026-07-16 15:38:29 +0530

    Update ReverseAString.py

--- ReverseAString.py
+++ ReverseAString.py
@@ -55,6 +55,33 @@ print(reversedString.strip())
 ##########################################################
 
 
+# 4 ) reverse all other characters except special characters
+import re
+
+input = "a@a#1c$d"
+rev = ""
+expectedOutput = ""
+count = 0
+
+# expected output = "d@c#1a$a"
+newInput = re.sub("[^a-zA-Z0-9]","",input)
+
+i = len(newInput)-1
+while i>=0:
+	rev = rev + newInput[i]
+	i -= 1
+print(rev)
+
+for j in range(len(input)):
+	if input[j] in newInput:
+		expectedOutput = expectedOutput + rev[count]
+		count += 1
+	else:
+		expectedOutput = expectedOutput + input[j]
+
+print(expectedOutput)
+
+
 
 
 
